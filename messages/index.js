29edbfd0694db3c56634b703a04d4898058c3041
Justin Chizer <justin.chizer@gmail.com>
2017-08-18 14:01:19 -0700

    LUIS Addition

--- messages/index.js
+++ messages/index.js
@@ -18,7 +18,7 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
 var bot = new builder.UniversalBot(connector);
 bot.localePath(path.join(__dirname, './locale'));
 
-/*var recognizer = new builder_cognitiveservices.QnAMakerRecognizer({
+var recognizer = new builder_cognitiveservices.QnAMakerRecognizer({
                 knowledgeBaseId: process.env.QnAKnowledgebaseId, 
     subscriptionKey: process.env.QnASubscriptionKey});
 
@@ -26,17 +26,46 @@ var basicQnAMakerDialog = new builder_cognitiveservices.QnAMakerDialog({
     recognizers: [recognizer],
                 defaultMessage: 'No match! Try changing the query terms!',
                 qnaThreshold: 0.3}
-);*/
+);
+var data = {}
+var options
+
+bot.dialog('/', dialog)
+
+//LUIS
+
+dialog.matches('greeting', [
+    function (session, args, next) {
+        session.send('Welcome to Green Machine, I am Patricia the chat bot assistant!')
+        session.beginDialog('/mainMenu', args)
+    }
+])
+
+dialog.matches('quote', [
+    function(session, args, next){
+        session.beginDialog('/Quote', args)
+    }
+])
+
+bot.dialog('/mainMenu', [
+    function(session) {
+        builder.Prompts.choice(session, ' What can I help you with today'['Get a Quote', 'Schedule Appointment'])
+    },
+    function (session, results) {
+        switch (results.response.index) {
+            case 0:
+
+            session.beginDialog('/Quote')
+            break
+
+            case 1:
+            session.beginDialog('/Schedule')
+            break
+        }
+    }
+])
 
 
-bot.dialog('/', [
-    function (session){
-        builder.Prompts.text(session, 'Hello, what is your name?');
-    },
-    function (session, result){
-        session.send('Hi ' + result.response + '! What is your phone number? (Please enter in xxx-xxx-xxxx format)');
-    },
-    ]);
 
 if (useEmulator) {
     var restify = require('restify');
